e2516945f30b928f02a6e2145ccf2cfab86a9f00
CrispenGari <Crispengari@gmail.com>
2021-10-01 15:05:23 +0200

    js-bug

--- src/utils/router.js
+++ src/utils/router.js
@@ -1,6 +1,6 @@
-import { Router } from "express";
+const  express= require("express") ;
 
-const router = Router();
+const router = express.Router();
 
 router.get("/", (req, res) => {
   res.status(200).json({
